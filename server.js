33aa92d9f9145e4bf867e3f4bfe4e158f5fe9323
kishor <153196042+kaushal-kumar-2109@users.noreply.github.com>
2026-05-13 12:38:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,107 +1,101 @@
 const express = require("express");
 const http = require("http");
-const { Server } = require("socket.io");
 const cors = require("cors");
+const { Server } = require("socket.io");
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: "*",
+  methods: ["GET", "POST"]
+}));
+
 app.use(express.json());
+app.use(express.static("public"));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
-    cors: {
-        origin: "*",
-    },
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"]
+  },
+  transports: ["websocket", "polling"]
 });
 
 const devices = {};
 const webClients = {};
 
 io.on("connection", (socket) => {
-    console.log("New Connection:", socket.id);
+  console.log("New Connection:", socket.id);
 
-    socket.on("register-device", (data) => {
-        const deviceCode =
-            typeof data === "string" ? data : data.deviceCode;
+  socket.on("register-device", (data) => {
+    const deviceCode = typeof data === "string" ? data : data.deviceCode;
+    devices[deviceCode] = socket.id;
 
-        devices[deviceCode] = socket.id;
+    console.log("Device Registered:", deviceCode);
+    console.log(devices);
+  });
 
-        console.log("Device Registered:", deviceCode);
-        console.log("All Devices:", devices);
-    });
+  socket.on("check-device", (deviceCode, callback) => {
+    if (devices[deviceCode]) {
+      webClients[socket.id] = deviceCode;
+      callback({ success: true });
+    } else {
+      callback({ success: false });
+    }
+  });
 
-    socket.on("check-device", (deviceCode, callback) => {
-        if (devices[deviceCode]) {
-            webClients[socket.id] = deviceCode;
+  socket.on("request-files", () => {
+    const deviceCode = webClients[socket.id];
+    const deviceSocketId = devices[deviceCode];
 
-            callback({
-                success: true,
-            });
-        } else {
-            callback({
-                success: false,
-            });
-        }
-    });
+    if (deviceSocketId) {
+      io.to(deviceSocketId).emit("get-files", {
+        webSocketId: socket.id
+      });
+    }
+  });
 
-    socket.on("request-files", () => {
-        const deviceCode = webClients[socket.id];
-        const deviceSocketId = devices[deviceCode];
+  socket.on("send-files-list", ({ webSocketId, files }) => {
+    io.to(webSocketId).emit("files-list", files);
+  });
 
-        if (deviceSocketId) {
-            io.to(deviceSocketId).emit("get-files", {
-                webSocketId: socket.id,
-            });
-        }
-    });
+  socket.on("request-file-data", ({ fileId }) => {
+    const deviceCode = webClients[socket.id];
+    const deviceSocketId = devices[deviceCode];
 
-    socket.on("request-file-data", ({ fileId }) => {
-        const deviceCode = webClients[socket.id];
-        const deviceSocketId = devices[deviceCode];
+    if (deviceSocketId) {
+      io.to(deviceSocketId).emit("get-file-data", {
+        webSocketId: socket.id,
+        fileId
+      });
+    }
+  });
 
-        if (deviceSocketId) {
-            io.to(deviceSocketId).emit("get-file-data", {
-                webSocketId: socket.id,
-                fileId,
-            });
-        }
-    });
+  socket.on("send-file-data", ({ webSocketId, file }) => {
+    io.to(webSocketId).emit("file-data", file);
+  });
 
-    socket.on("send-file-data", ({ webSocketId, file }) => {
-        io.to(webSocketId).emit("file-data", file);
-    });
+  socket.on("disconnect", () => {
+    delete webClients[socket.id];
 
-    socket.on("send-files-list", ({ webSocketId, files }) => {
-        io.to(webSocketId).emit("files-list", files);
-    });
+    for (let code in devices) {
+      if (devices[code] === socket.id) {
+        delete devices[code];
+      }
+    }
 
-    socket.on("disconnect", () => {
-        console.log("Disconnected:", socket.id);
-
-        delete webClients[socket.id];
-
-        for (let code in devices) {
-            if (devices[code] === socket.id) {
-                delete devices[code];
-            }
-        }
-    });
+    console.log("Disconnected:", socket.id);
+  });
 });
 
-app.use(express.json());
-app.use(express.static("public"));
-
-app.get("/", (req, res) => {
-    res.send("Backend Running");
+app.get("/health", (req, res) => {
+  res.send("Backend running");
 });
 
-server.listen(5000, () => {
-    console.log("Server Running On Port 5000");
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
 });
-
-
-
-
-// https://expo.dev/accounts/kaushal21092003/projects/System/builds/9354a98f-d394-403d-8bb9-06adbdb48660
